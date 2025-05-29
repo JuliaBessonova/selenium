00512ccb21879161006a76fa136a86877eef38b7
Julia Bessonova <bessonova.jul@gmail.com>
2025-05-29 23:38:49 +0300

    login as admin test added

--- Tests/test_example.py
+++ Tests/test_example.py
@@ -1,21 +1,18 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
-    print(wd.capabilities)
+    # print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
 
-def test_example(driver):
-    driver.get("http://www.google.com/")
-
-
-##    driver.find_element_by_name("q").send_keys("webdriver")
-##    driver.find_element_by_name("btnG").click()
-##    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
+def test_login_as_admin(driver):
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element(By.NAME, "username").send_keys("admin")
+    driver.find_element(By.NAME, "password").send_keys("admin")
+    driver.find_element(By.NAME, "login").click()
