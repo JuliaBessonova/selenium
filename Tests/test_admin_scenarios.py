116ac4d864100a0e010d69c24c9f4c8df2265652
Julia Bessonova <bessonova.jul@gmail.com>
2025-06-13 21:24:45 +0300

    задание 6. добавлены исправления

--- Tests/test_admin_scenarios.py
+++ Tests/test_admin_scenarios.py
@@ -24,145 +24,30 @@ def find_title(driver):
 
 
 def test_check_admin_navigation_menu(driver):
-    login_as_admin(driver)
+    login_as_admin(driver, "http://localhost/litecart/admin/")
 
-    driver.find_element(By.LINK_TEXT, "Appearence").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-template").click() 
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-logotype").click()
-    find_title(driver)
+    sections = driver.find_elements(By.CSS_SELECTOR, "ul#box-apps-menu li span.name")
+    names = []
 
-    driver.find_element(By.LINK_TEXT, "Catalog").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-catalog").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-product_groups").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-option_groups").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-manufacturers").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-suppliers").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-delivery_statuses").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-sold_out_statuses").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-quantity_units").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-csv").click()
-    find_title(driver)
+    for section in sections:
+        names.append(section.text)
 
-    driver.find_element(By.LINK_TEXT, "Countries").click()
-    find_title(driver)
+    for name in names:
+        driver.find_element(By.LINK_TEXT, name).click()
+        find_title(driver)
 
-    driver.find_element(By.LINK_TEXT, "Currencies").click()
-    find_title(driver)
+        subs = driver.find_elements(By.CSS_SELECTOR, "ul.docs li span.name")
+        subs_names = []
+        if subs != []:
+            for sub in subs:
+                subs_names.append(sub.text)
 
-    driver.find_element(By.LINK_TEXT, "Customers").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-customers").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-csv").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-newsletter").click()
-    find_title(driver)
+        if subs_names != []:
+            for name in subs_names:
+                driver.find_element(By.LINK_TEXT, name).click()
+                find_title(driver)
 
-    driver.find_element(By.LINK_TEXT, "Geo Zones").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Languages").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-languages").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-storage_encoding").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Modules").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-jobs").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-customer").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-shipping").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-payment").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-order_total").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-order_success").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-order_action").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Orders").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-orders").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-order_statuses").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Pages").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Reports").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-monthly_sales").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-most_sold_products").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-most_shopping_customers").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Settings").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-store_info").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-defaults").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-general").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-listings").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-images").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-checkout").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-advanced").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-security").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Slides").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Tax").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-tax_classes").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-tax_rates").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Translations").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-search").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-scan").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-csv").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "Users").click()
-    find_title(driver)
-
-    driver.find_element(By.LINK_TEXT, "vQmods").click()
-    find_title(driver)
-    driver.find_element(By.CSS_SELECTOR, "li#doc-vqmods").click()
-    find_title(driver)
-
-
-
+    
 def test_check_countries_and_zones_lists(driver):
     login_as_admin(driver, "http://localhost/litecart/admin/?app=countries&doc=countries")
     rows = driver.find_elements(By.CSS_SELECTOR, "tr.row")
@@ -207,3 +92,4 @@ def test_check_country_zones_sorting(driver):
             zone_list.append(zone.text)
 
         assert zone_list == sorted(zone_list), f"The list of zones {zone_list} is not alphabetically sorted"
+
